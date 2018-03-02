ccc7a02818e0f1279bfa2cdb48e0bf3e12b092da
Jiphi <novitz@gmail.com>
2018-03-03 00:40:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+      <div class="container">
+        <div class="row">
+      <h1 class="col-lg-12 text-center">La Clémentine</h1>
+      <h2 class="text-center">Infos allergies</h2>
+        </div>
+      <div class="row">
+    <allergens/>
+      </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Allergens from './components/Allergens'
+
+export default {
+  name: 'App',
+  components: {
+    Allergens
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Allergens.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="allergens inline">
+    <div class="col-lg-12  text-left">
+        <div class="">
+          <div v-for="allergen in allergens"  >
+            <input type="checkbox" :id="allergen" :value="allergen" v-model="list" class="" >
+            <label :for="allergen">{{allergen}}</label>
+          </div>
+        </div>
+    </div>  <!-- col 2-->
+      <div class="col-lg-7">
+        <ul>
+          <li v-for="product in filtered">{{product.name}}</li>
+        </ul>
+      </div> <!-- / col-10-->
+  </div> <!-- / allergnes-->
+</template>
+
+<script>
+export default {
+  name: 'Allergens',
+  data () {
+    return {
+      allergens: [
+        "Céleri",
+        "Oeufs",
+        "Gluten",
+        "Crustacés",
+        "Poisson",
+         "Lupin",
+        "Mollusque","Moutarde",
+        "Fruit à coque",
+        "Arachides",
+        "Graines de Sésame",
+        "Sulfites",
+        "Soja",
+      ],
+      products: [
+        {name: "Salade Composée", allergens: ["Graines de Sésame", "Arachides"]},
+        {name: "Coeur de Clem", allergens: ["Gluten", "Graines de Sésame"]},
+        {name: "Le Tonata", allergens: ["Oeufs", "Graines de Sésame", "Gluten","Poisson"]},
+        {name: "Le Célèbre Dago", allergens: ["Oeufs", "Graines de Sésame", "Gluten", "Lait"]},
+        {name: "Le Pain de viande", allergens: ["Oeufs", "Gluten", "Arachide"]},
+        {name: "Le Fish", allergens: ["Poisson", "Gluten", "Lait"]},
+        {name: "Le Seguin", allergens: ["Gluten", "Lait", "Arachide"]},
+        {name: "Le Poulet Cocktail", allergens: ["Oeufs", "Gluten"]},
+        {name: "Le Poulet basilic", allergens: ["Oeufs", "Gluten"]},
+        {name: "Le Poulet chorizo", allergens: ["Oeufs", "Gluten"]},
+        {name: "Le Thon piquant maison", allergens: ["Oeufs", "Gluten", "Poisson"]},
+        {name: "Le Poulet basilic", allergens: ["Oeufs", "Gluten"]},
+       
+
+      ],
+      list: []
+    }
+  },computed: {
+    filtered: function(){
+      /*return this.products.filter(p => this.list.indexOf(p.allergens) === -1)*/
+      var list= this.list
+      var products = this.products
+      var test = null
+      var got = []
+      products.map(function (product){
+        let allergens = product.allergens
+       console.log("*************"+list)
+       console.log("---------"+allergens) 
+        test = allergens.filter(function(allergen){
+          return (list.indexOf(allergen) > -1)
+        })
+        if(test.length>0){
+          got.push(product)
+        }
+       
+               
+
+      })
+      return got
+    }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  /*display: inline-block;*/
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.inline{
+  display: inline-flex;
+}
+.checkbox-wrapper {
+  position: relative;
+  top: -1px;
+}
+
+</style>
